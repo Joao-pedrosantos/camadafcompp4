8b82c025881c043398d532a15764974e8882ccb5
Matheus <matheusc1@al.insper.edu.br>
2023-03-09 14:48:59 -0300

    i reject society

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 def Datagrama(tipo, npacks=00, num_pack=00, file_id=00, payload_len=00, error_pack=00, last_pack=00, crc=00, payload=b''):
     eop = b'\xAA\xBB\xCC\xDD'
+    final = payload + eop
     if tipo == 1:#é o handshake
-        mensagem = (bytes([tipo, 69, 00, npacks, num_pack, file_id, error_pack, last_pack, crc, crc]) + ((payload) + (eop)))
+        mensagem = (bytes([tipo, 69, 00, npacks, num_pack, file_id, error_pack, last_pack, crc, crc]) + (final))
         
     else:
-        mensagem = (bytes([tipo, 00, 00, npacks, num_pack, payload_len+1, error_pack, last_pack, crc, crc]) + ((payload) + (eop)))
+        mensagem = (bytes([tipo, 00, 00, npacks, num_pack, payload_len+1, error_pack, last_pack, crc, crc]) + (final))
 
     return mensagem
     

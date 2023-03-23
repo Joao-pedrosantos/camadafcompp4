6340a23ce58ab60d9511473eb141a8b2b6ec5e9f
João Pedro <joaoprs3@al.insper.edu.br>
2023-03-23 15:08:06 -0300

    skidibpaoópo

--- utils.py
+++ utils.py
@@ -4,7 +4,9 @@ def Datagrama(tipo="", npacks=00, num_pack=00, file_id=00, payload_len=00, error
     eop = b'\xFF\xAA\xFF\xAA'
     crc = zlib.crc32(payload)
     #transforma o crc em bytes
-    crc = crc.to_bytes(2, byteorder='big')
+    crc = crc.to_bytes(4, byteorder='big')
+    crc1 = crc[0:1]
+    crc2 = crc[2:3]
 
     if tipo == "1":        
         mensagem = [1, 00, 15, npacks, num_pack, file_id, error_pack, last_pack, 00, 00]
@@ -17,7 +19,7 @@ def Datagrama(tipo="", npacks=00, num_pack=00, file_id=00, payload_len=00, error
         mensagem += payload
         mensagem += eop
     else:
-        mensagem = [int(tipo[0]), 00, 00, npacks, num_pack, payload_len, error_pack, last_pack, crc, 00]
+        mensagem = [int(tipo[0]), 00, 00, npacks, num_pack, payload_len, error_pack, last_pack, crc1, crc2]
         mensagem = bytes(mensagem)
         mensagem += payload
         mensagem += eop
